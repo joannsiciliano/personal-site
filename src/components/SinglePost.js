9a06f6c684e9957c62161737ab2f83d6721d66b7
JoAnn R Siciliano <77905621+joannsiciliano@users.noreply.github.com>
2021-07-01 15:38:38 -0600

    single post functional

--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -1,5 +1,73 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import sanityClient from "../client.js";
+import { useParams } from "react-router-dom";
+import imageUrlBuilder from "@sanity/image-url";
+import BlockContent from "@sanity/block-content-to-react";
+
+const builder = imageUrlBuilder(sanityClient);
+function urlFor(source) {
+  return builder.image(source);
+}
 
 export default function SinglePost() {
-  return <h1>Single Post page!</h1>;
+  const [singlePost, setSinglePost] = useState(null);
+  const { slug } = useParams();
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[slug.current == "${slug}"]{
+      title,
+      _id,
+      slug,
+      mainImage{
+        asset->{
+          _id,
+          url
+        }
+      },
+      body,
+      "name":author->name,
+      "authorImage": author->image
+    }`
+      )
+      .then((data) => setSinglePost(data[0]))
+      .catch(console.error);
+  }, [slug]);
+  if (!singlePost) return <div> Loading...</div>;
+  return (
+    <main className="MainSinglePost">
+      <article className="ArticleSinglePost">
+        <header className="HeaderSinglePost">
+          <div className="Div1SinglePost">
+            <div className="Div1SinglePost">
+              <h1 className="H1SinglePost"> {singlePost.title}</h1>
+              <div className="Div3SinglePost">
+                <img
+                  src={urlFor(singlePost.authorImage).url()}
+                  alt={singlePost.name}
+                  className="SinglePostImage"
+                />
+              </div>
+              <p className="SinglePostParagraph">{singlePost.name}</p>
+            </div>
+          </div>
+          <img
+            src={singlePost.mainImage.asset.url}
+            alt={singlePost.title}
+            className="SinglePostImage2"
+            style={{ height: "400px" }}
+          />
+        </header>
+        <div className="SinglePostBlockContent">
+          {" "}
+          <BlockContent
+            blocks={singlePost.body}
+            projectId="9d1fo0b1"
+            dataset="production"
+          />
+        </div>
+      </article>
+    </main>
+  );
 }
