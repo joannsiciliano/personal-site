773b1d3c88a0fa5196e17b302775336e8cdc02e1
JoAnn R Siciliano <77905621+joannsiciliano@users.noreply.github.com>
2021-07-02 14:16:31 -0600

    social media links added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,19 @@
 import React from "react";
+import HomeCSS from "../cssModules/Home.module.css";
 
 export default function Home() {
-  return <h1>Siciliano.dev</h1>;
+  return (
+    <div className={HomeCSS.HomePage}>
+      <section className={HomeCSS.LinkSection}>
+        <a href="https://www.linkedin.com/in/joannsiciliano/">LinkedIn</a>
+        <a href="https://github.com/joannsiciliano">Github</a>
+        <a href="https://www.instagram.com/j_s.jpg/">Instagram</a>
+        <a href="https://www.instagram.com/j_s.jpg/">Twitter</a>
+        <a href="mailto:joannsiciliano.dev@gmail.com">Email</a>
+      </section>
+      <div className={HomeCSS.NameDiv}>
+        <h1>Hi! I'm Jo.</h1>
+      </div>
+    </div>
+  );
 }
